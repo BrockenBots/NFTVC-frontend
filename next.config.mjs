8f5c23baa0eb4a07e1444f647abff877018fb5dc
ASIFMCjr <88495461+ASIFMCjr@users.noreply.github.com>
2024-10-25 02:30:51 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/",
-        permanent: true,
-      },
-    ];
-  },
-};
+  async redirects() {};
 
 export default nextConfig;
