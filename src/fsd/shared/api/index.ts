e137f4db41f96291c3d82fead8c8909e453495f6
ASIFMCjr <88495461+ASIFMCjr@users.noreply.github.com>
2024-10-25 02:52:34 +0300

    Update index.ts

--- src/fsd/shared/api/index.ts
+++ src/fsd/shared/api/index.ts
@@ -1,10 +1,16 @@
 import { Api as AuthApi } from "./typed-api/AuthApi";
 import { Api as ProfilesApi } from "./typed-api/ProfilesApi";
 
+const getAccessToken = () => localStorage.getItem("access_token") || "";
 export const authApi = new AuthApi({
   baseUrl: "http://82.97.253.57:81",
 });
 
 export const profilesApi = new ProfilesApi({
   baseUrl: "http://82.97.253.57:81",
+  baseApiParams: {
+    headers: {
+      Authorization: `Bearer ${getAccessToken()}`,
+    },
+  },
 });
